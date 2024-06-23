c84e9d25b1cb182cf2bf0e447eae9401ff59afe4
s27643 <s27643@pjwstk.edu.pl>
2024-06-23 18:00:11 +0200

    added files

--- WebApplication1/Controllers/HelloWorldController.cs
+++ WebApplication1/Controllers/HelloWorldController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication1.Controllers;
 
 public class HelloWorldController : Controller
 {
-    // GET
+    // 
+    // GET: /HelloWorld/
     public IActionResult Index()
     {
+        return View();
+    }
+    
+    // 
+    // GET: /HelloWorld/Welcome/ 
+    public IActionResult Welcome(string name, int numTimes = 1)
+    {
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
         return View();
     }
 }
--- WebApplication1/Models/MovieGenreViewModel.cs
+++ WebApplication1/Models/MovieGenreViewModel.cs
@@ -1,6 +1,11 @@
-﻿namespace WebApplication1.Models;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WebApplication1.Models;
 
 public class MovieGenreViewModel
 {
-    
+    public List<Movie>? Movies { get; set; }
+    public SelectList? Genres { get; set; }
+    public string? MovieGenre { get; set; }
+    public string? SearchString { get; set; }
 }
